b00ef1967a5dae96307ed159ba301ac070d00d5a
Joe <josephlhartley@icloud.com>
2021-11-11 14:17:11 +0000

    added debug statements

--- roam.py
+++ roam.py
@@ -94,17 +94,19 @@ def main():
 
             if pi2go.irCentre():
                 print("Detected a wall! moving back and turning.")
-                sleep(2)
+                sleep(1)
                 pi2go.go(0, 0)
                 reverseTurn()
                 pid.setpoint = head.heading()
             elif pi2go.irLeft():
                 print("Detected a wall! moving back and turning.")
+                sleep(1)
                 pi2go.go(0, 0)
                 reverseTurn()
                 pid.setpoint = head.heading()
             elif pi2go.irRight():
                 print("Detected a wall! moving back and turning.")
+                sleep(1)
                 pi2go.go(0, 0)
                 reverseTurn()
                 pid.setpoint = head.heading()
