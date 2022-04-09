76cdaebb63303938db30b62d613a6fd64bb1ac4a
dronejoe <josephlhartley@icloud.com>
2022-04-09 16:51:36 +0100

    removed use of pandas

--- testICM.py
+++ testICM.py
@@ -1,23 +1,29 @@
 from icm20948 import ICM20948
-import pandas as pd
+# import pandas as pd
 import math
 
 def main():
     imu = ICM20948()
     n = 0
-    mag =[]
-    df = pd.DataFrame(columns=['id', 'X', 'Y', 'Z', 'X1', 'Y1', 'Z1', 'atan2'])
+    magData =[]
+    # df = pd.DataFrame(columns=['id', 'X', 'Y', 'Z', 'X1', 'Y1', 'Z1', 'atan2'])
     try:
         while True:
             mag = list(imu.read_magnetometer_data())
             heading = math.atan2(mag[1], mag[2])
-            item = pd.DataFrame({'id': n, 'X': mag[0], 'Y':mag[1], 'Z':mag[2], 'X1':mag[2], 'Y1':mag[1], 'Z1':mag[0], 'atan2':heading})
-            df = pd.concat(item, df)
+            # item = pd.DataFrame({'id': n, 'X': mag[0], 'Y':mag[1], 'Z':mag[2], 'X1':mag[2], 'Y1':mag[1], 'Z1':mag[0], 'atan2':heading})
+            # df = pd.concat(item, df)
             print(f"{n}, {mag[0]}, {mag[1]}, {mag[2]}, {heading}")
+            magData.append(f"{n}, {mag[0]}, {mag[1]}, {mag[2]}, {heading}\n")
             n += 1
 
     except KeyboardInterrupt:
-        df.to_csv('testICMNorth.csv')
+        # df.to_csv('testICMNorth.csv')
+        with open("testICMNorth.csv", "w") as f:
+            for line in magData:
+                f.write(line)
+
+
 
 if __name__ == "__main__":
     main()
