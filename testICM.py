e8869a371847fcd8e24ddb5a0d76a4de4ca1c418
Andy Hartley <andrew.hartley@metoffice.gov.uk>
2022-04-11 16:31:55 +0100

    Added IMU calibration code

--- testICM.py
+++ testICM.py
@@ -1,6 +1,23 @@
 from icm20948 import ICM20948
 # import pandas as pd
 import math
+import os
+
+def getCalibration():
+
+    file = 'heading/calibration.txt'
+    if not os.path.isfile():
+        # Run calibration
+    else:
+        cal = {}
+        with open("heading/calibration.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                key, val = line.split(':')
+                cal[key] = val.strip('\n')
+
+    return cal
+
 
 def main():
     imu = ICM20948()
@@ -9,6 +26,10 @@ def main():
     # df = pd.DataFrame(columns=['id', 'X', 'Y', 'Z', 'X1', 'Y1', 'Z1', 'atan2'])
     try:
         while True:
+            # NB: mag output is
+            # mag[0] = Z
+            # mag[1] = Y
+            # mag[2] = X
             mag = list(imu.read_magnetometer_data())
             heading = math.atan2(mag[1], mag[2])
             # item = pd.DataFrame({'id': n, 'X': mag[0], 'Y':mag[1], 'Z':mag[2], 'X1':mag[2], 'Y1':mag[1], 'Z1':mag[0], 'atan2':heading})
