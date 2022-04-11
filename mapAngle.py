53f1d54d7f791e24939c0e280841d8419a147680
dronejoe <josephlhartley@icloud.com>
2022-04-11 13:15:50 +0100

    updated mapAngle.py and plotScatter.py

--- mapAngle.py
+++ mapAngle.py
@@ -4,37 +4,42 @@
 # Description: Will create a csv file of the angles as the pi2go rotates
 # ****************************************************
 
+# Imports the required packages
 import pi2go
-import headingICM20948
 import time
 from icm20948 import ICM20948
 
+# Initialises pi2go and imu code
 pi2go.init()
-# head = headingICM20948.CompassHeading()
 imu = ICM20948()
 
+# Declares required variables
 mag = []
-# angles = []
 n = 0
 
 try:
+    # Turns pi2go at a constant speed clockwise
     pi2go.spinRight(80)
+
+    # Takes raw magnetometer data and appends to mag list
     init_data = list(imu.read_magnetometer_data())
     init_data.append(n)
     mag.append(init_data)
-    # angles.append(head.getHeading())
-    # mag.append(head.getMag())
+
+    # Loops data collection forever
     while True:
-        # angles.append(head.getHeading())
+        n += 1
         data = list(imu.read_magnetometer_data())
         data.append(n)
         mag.append(data)
-        n += 1
         time.sleep(0.01)
 
+# On KeyboardInterrupt, run following:
 except KeyboardInterrupt:
+    # Stop both motors
     pi2go.go(0,0)
 
+    # Write raw data to csv in format: n, x, y, z
     with open('test360Spin.csv','w') as f:
         for i in mag:
             f.write(f"{i[3]}, {i[0]}, {i[1]}, {i[2]}\n")
--- plotScatter.py
+++ plotScatter.py
@@ -1,7 +1,90 @@
+# ****************************************************
+# Filename: plotScatter.py
+# Creater: Joe
+# Description: Plots a series of graphs from raw mag data
+# ****************************************************
+
+# Imports required packages
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import math
 
-df = pd.read_csv('D:/Personal files/Documents/testICM_scatterplot2.txt')
-df["x1"] = df['x'] * -1
-df["y1"] = df['y'] * -1
-sns.scatterplot(data=df, x='x1', y='y1', hue='heading')
+# Reads data from csv files and outputs a pandas dataframe
+def readData(name="test360Spin.csv"):
+    if name == "NSEW":
+        df = pd.read_csv("D:/Personal files/Documents/testICM_scatterplot2.txt")
+    else:
+        df = pd.read_csv(name)
+    return df
+
+# Plots a graph and groups data by heading (either N, S, E or W)
+def plotNSEW(df):
+    sns.set_theme(style="whitegrid")
+    g = sns.scatterplot(data=df, x='x1', y='y1', hue='heading')
+    g.set_title("Scatter plot of Raw Mag Data at NSEW")
+    plt.savefig("D:/Personal files/Documents/scatterNSEW.png")
+
+# Plots a scatter graph
+def plotCircleScatter(df, hue=None,title=None, filename=None):
+    sns.set_theme(style="whitegrid")
+    if hue:
+        g = sns.scatterplot(data=df, x='x1', y='y1',hue=hue, cmap="viridis")
+    else:
+        g = sns.scatterplot(data=df, x='x1', y='y1')
+    g.set_title(title)
+    plt.savefig(f"D:/Personal files/Documents/{filename}")
+
+# Calibrates input dataframe
+def calibrateData(df):
+    df["x1"] = df['x'] * -1
+    df["y1"] = df['y'] * -1
+
+    xmax = df["x1"].max()
+    xmin = df["x1"].min()
+    ymax = df["y1"].max()
+    ymin = df["y1"].min()
+
+    df["x1"] -= xmin
+    df["x1"] /= (xmax - xmin)
+    df["x1"] -= 0.5
+
+
+    df["y1"] -= ymin
+    df["y1"] /= (ymax - ymin)
+    df["y1"] -= 0.5
+
+    return df
+
+# Calculates heading from dataframe
+def headingCalc(df):
+    df["heading"] = [math.degrees(math.atan2(df["y1"].to_numpy()[i], df["x1"].to_numpy()[i])) for i in np.arange(len(df["x1"]))]
+    # We want the heading, which is the angle from North which, in our case, lies on the Y axis.
+    # Atan2 takes the arguments atan2(y, x) to find the angle from the X axis. However, we want the angle from the Y axis so we switch it around.
+
+    return df
+
+# Main function
+def main():
+    df = readData("NSEW")
+    df["x1"] = df['x'] * -1
+    df["y1"] = df['y'] * -1
+    plotNSEW(df)
+
+    df = readData("test360Spin.csv")
+    df["x1"] = df['x']
+    df["y1"] = df['y']
+    plotCircleScatter(df, title="Scatter plot Uncalibrated Data", filename="uncalibratedDataSctter.png")
+    df = calibrateData(df)
+    plotCircleScatter(df,title="Scatter plot Calibrated Data",filename="calibratedDataSctter.png")
+
+    df = headingCalc(df)
+    plotCircleScatter(df, hue="heading", title="Scatter plot Calibrated Data with Heading", filename="calibratedDataSctterHeading.png")
+
+    plotCircleScatter(calibrateddf, hue="heading", title="Scatter plot Calibrated Data with Heading",
+                      filename="calibratedDataSctterHeading.png")
+
+
+if __name__ == "__main__":
+    main()
