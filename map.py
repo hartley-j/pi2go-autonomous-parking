e91e7b50fc238f164d8b4c8d9601892024fca82d
dronejoe <josephlhartley@icloud.com>
2022-04-12 20:26:26 +0100

    added getCoordinate to map.py

--- map.py
+++ map.py
@@ -3,13 +3,13 @@
 # Creater: Joe
 # Description: Program to find the equation of the four walls around the robot
 # ****************************************************
-
+import math
 import numpy as np
 import robot
 import heading
 import pi2go
 
-def main():
+def collectData(filename=None):
     rob = robot.Robot()
     head = heading.Compass()
     initHeading = head.meanAngle([head.getHeading() for i in np.arange(10)])
@@ -31,10 +31,52 @@ def main():
         angles.append((currentHeading, pi2go.getDistance()))
         n += 1
 
-    with open("map.txt", "w") as file:
-        for line in angles:
-            file.write(f"{line}\n")
+    if filename:
+        with open(filename, "w") as file:
+            for line in angles:
+                file.write(f"{line}\n")
 
+    return angles
+
+def getCoordinates(angles, readfile='map.txt', writefile='coordinates.txt'):
+    coordinates = []
+
+    if not angles:
+        with open(readfile, 'r') as file:
+            for line in file:
+                angles.append(line)
+
+    for i in angles:
+        angle = i[0]
+        distance = i[1]
+        xCoord, yCoord = 0, 0
+
+        if 0 <= angle <= 90:  # In +x and +y quadrant
+            theta = angle
+            yCoord = distance * math.sin(theta)
+            xCoord = distance * math.cos(theta)
+        elif 90 < angle <= 180:  # In +x and -y quadrant
+            theta = angle - 90
+            yCoord = -(distance * math.sin(theta))
+            xCoord = distance * math.cos(theta)
+        elif 0 > angle >= -90:  # In the -x and +y quadrant
+            theta = abs(angle)
+            yCoord = distance * math.sin(theta)
+            xCoord = -(distance * math.cos(theta))
+        elif -90 > angle >= -180:  # In the -x and -y quadrant
+            theta = abs(angle + 90)
+            yCoord = -(distance * math.sin(theta))
+            xCoord = -(distance * math.cos(theta))
+
+        coordinates.append((xCoord, yCoord))
+
+    with open(writefile, 'w') as file:
+        for coord in coordinates:
+            file.write(f"{coord}\n")
+
+    return coordinates
 
 if __name__ == '__main__':
-    main()
+    data = collectData("map.txt")
+    coords = getCoordinates(data)
+
