687b5f61ce05a663b8a74f90c184e9b192cbd23f
dronejoe <josephlhartley@icloud.com>
2022-04-12 10:40:56 +0100

    improved robot.py and added normaliseHeading to __init__.py

--- robot.py
+++ robot.py
@@ -25,7 +25,7 @@ class Robot:
         head = self.heading.getHeading()
         currentDistance = pi2go.getDistance()
 
-        pid = PID(1, 0.1, 0, setpoint=head)
+        pid = PID(1, 0, 0, setpoint=head)
         pid.output_limits = (0, 100)
 
         changeDistance = currentDistance - distance
