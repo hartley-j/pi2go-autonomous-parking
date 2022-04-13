2b7d737e01e9aac8a256a9370ac476c91dea06da
dronejoe <josephlhartley@icloud.com>
2022-04-13 15:56:14 +0100

    change pid values

--- robot.py
+++ robot.py
@@ -30,7 +30,7 @@ class Robot:
         head = round(self.heading.getHeading())
         currentDistance = pi2go.getDistance()
 
-        pid = PID(0.3, 0.1, 0.1, setpoint=head)
+        pid = PID(0.3, 0.4, 0.1, setpoint=head)
         pid.output_limits = (-(100 + speed), (100 - speed))
 
         changeDistance = currentDistance - distance
