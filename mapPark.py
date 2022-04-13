5177ccbaad7e01d33d6dab9e2aa2c003e4f9b8af
dronejoe <josephlhartley@icloud.com>
2022-04-13 15:48:45 +0100

    debug mapPark.py

--- mapPark.py
+++ mapPark.py
@@ -9,10 +9,12 @@ def main():
     widthWheels = 2.6
 
     initDistance = pi2go.getDistance() + lengthFrontToBack
-    rob.forward(50, initDistance/2)
+    print(f"Init distance: {initDistance} \t")
+    rob.forward(60, initDistance/2)
     distance = pi2go.getDistance()
-    if not ((initDistance/2) -3) < distance < ((initDistance/2) + 3):
-        rob.forward(40,(initDistance/2) - distance)
+    print(f"Final distance: {distance} \n")
+    # if not ((initDistance/2) -3) < distance < ((initDistance/2) + 3):
+    #     rob.forward(40,(initDistance/2) - distance)
 
 if __name__ == '__main__':
     main()
