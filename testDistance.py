fcbd759474b104bf032285482edea73d2087215b
dronejoe <josephlhartley@icloud.com>
2022-04-13 10:22:08 +0100

    added testDistance.py for testing the distance sensor

---
+++ testDistance.py
@@ -0,0 +1,28 @@
+import robot
+import pi2go
+from time import sleep
+
+
+def main():
+    """
+    Function to test the distance sensor
+    Pointing at a long wall and taking distance data for small increments of degrees
+    """
+    rob = robot.Robot()
+    pi2go.init()
+    distances = [pi2go.getDistance()]
+
+    for i in range(10):
+        distances.append(angleDistance(5 * i, rob))
+
+    return distances
+
+def angleDistance(degrees, rob):
+    rob.rotateAngle(degrees)
+    sleep(1)
+    return degrees, pi2go.getDistance()
+
+
+if __name__ == "__main__":
+    data = main()
+    print(data)
