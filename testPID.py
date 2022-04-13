ce545d57b4e0eb6af917cf9019d2f93b444f534d
dronejoe <josephlhartley@icloud.com>
2022-04-13 15:59:52 +0100

    change pid values

---
+++ testPID.py
@@ -0,0 +1,18 @@
+import robot
+import heading
+import pi2go
+
+def main():
+    rob = robot.Robot()
+    lengthFrontToBack = 14.3
+    width = 13.6
+    widthWheels = 2.6
+
+    initDistance = pi2go.getDistance() + lengthFrontToBack
+    print(f"Init distance: {initDistance} \t")
+    rob.forward(60, initDistance/2)
+    distance = pi2go.getDistance()
+    print(f"Final distance: {distance} \n")
+
+if __name__ == '__main__':
+    main()
