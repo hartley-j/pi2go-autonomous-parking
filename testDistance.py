ed95a2a1f820fda35a8c35f5e12ff30b15eb4900
dronejoe <josephlhartley@icloud.com>
2022-04-13 11:12:57 +0100

    added testDistance.py for testing the distance sensor

--- testDistance.py
+++ testDistance.py
@@ -12,14 +12,14 @@ def main():
     distances = [(0, pi2go.getDistance())]
 
     for i in range(10):
-        rob.rotateAngle(5, tolerance=1.5)
+        rob.rotateAngle(5, tolerance=2.5)
         sleep(1)
         distances.append((5 * (i+1), pi2go.getDistance()))
 
-    rob.rotateAngle(-100)
+    rob.rotateAngle(-50)
 
     for i in range(10):
-        rob.rotateAngle(5, tolerance=1.5)
+        rob.rotateAngle(-5, tolerance=2.5)
         sleep(1)
         distances.append((-50 + 5 * (i+1), pi2go.getDistance()))
 
