c6653a99267ed973304ba3f186a73e5111bb8243
dronejoe <josephlhartley@icloud.com>
2022-04-14 17:52:28 +0100

    updated angularSpeed.py

--- angularSpeed.py
+++ angularSpeed.py
@@ -6,17 +6,15 @@ def main():
     start = time.time()
 
     pi2go.go(0, -20)
-    time.sleep(10)
+    time.sleep(1)
 
-    # time.sleep(0.005)
-    #
-    # while not pi2go.irCentre():
-    #     pass
-    # pi2go.go(0, 0)
-    # print("Wall detected")
-    #
-    # end = time.time()
-    # print(f"Elapsed time: {end - start}")
+    while not pi2go.irCentre():
+        pass
+    pi2go.go(0, 0)
+    print("Wall detected")
+
+    end = time.time()
+    print(f"Elapsed time: {end - start}")
 
     pi2go.cleanup()
 
