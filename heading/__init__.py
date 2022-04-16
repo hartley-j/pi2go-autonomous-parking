d977ec606460c906cce322b545862f488bd92a55
dronejoe <josephlhartley@icloud.com>
2022-04-16 17:51:55 +0100

    added docstrings to __init__.py

--- heading/__init__.py
+++ heading/__init__.py
@@ -35,12 +35,13 @@ class Compass:
 
 
     def getMag(self):
-        '''
+        """
         Get mag values in form: [X, Y, Z]
         Raw output (from the sensor) is: X, Y, Z
         But, because of the orientation of the sensor, these values are ACTUALLY:
               Z1, Y1, X1
-        '''
+        :returns: Raw magnetometer data in form of list [X, Y, Z]
+        """
         Z1, Y1, X1 = list(self.imu.read_magnetometer_data())
         X1 *= -1
         Y1 *= -1
@@ -48,9 +49,10 @@ class Compass:
 
 
     def calibrate(self, raw):
-        '''
+        """
         Calibrates the raw magnetometer output by normalising between min and max values from the calibration.txt file
-        '''
+        :returns: calibrated raw data in form [x, y, z]
+        """
 
         # Normalises every axes value with respective min and max values
         for k, val in raw.items():
@@ -60,17 +62,18 @@ class Compass:
             raw[k] -= self.cal[f"{k}min"]
             # Divide by the range so that all values are scaled to between 0 and 1
             raw[k] /= (self.cal[f"{k}max"] - self.cal[f"{k}min"])
-            # Subtract 0.5 so that all values are between -0.5 and 0.5
+            # Subtract 0.5 so that all values are between -0.5 and 0.5
             raw[k] -= 0.5
 
         return raw
 
     def headingCalc(self, coord):
-        '''
+        """
         We want the heading, which is the angle from North which, in our case, lies on the Y axis.
         Atan2 takes the arguments atan2(y, x) to find the angle from the X axis.
         However, we want the angle from the Y axis so we switch x and y around.
-        '''
+        :returns: calculated heading as float
+        """
 
         heading = math.degrees(math.atan2(coord['x'], coord['y']))
         # TODO: Flip the heading (Currently, heading=0 is south, whereas it should be north and vice-versa)
@@ -78,9 +81,10 @@ class Compass:
         return heading
 
     def getHeading(self):
-        '''
+        """
         Gets data from the magnetometer, calibrates it, and calculates the heading
-        '''
+        :returns: current heading as float
+        """
 
         magData = self.getMag()
         magData = self.calibrate(magData)
@@ -89,9 +93,11 @@ class Compass:
         return heading
 
     def getMedianHeading(self, nmax=50):
-        '''
+        """
         Get a median angle from a sample of nmax
-        '''
+        :param nmax: number of samples taken of the heading
+        :returns: the median heading as float
+        """
         n = 0
         xlist, ylist = [[], []]
         while n < nmax:
@@ -108,7 +114,11 @@ class Compass:
         return heading_median
 
     def normaliseHeading(self, deg):
-        # Used in oldMap.py and robot.py
+        """
+        'Normalises' heading so that it is inbetween -180 and 180
+        :param deg: the heading to be normalised
+        :return: a normalised heading
+        """
         if deg > 180:
             deg -= 360
         if deg < -180:
@@ -116,8 +126,9 @@ class Compass:
         return deg
 
     def meanAngle(self, deg):
-        '''
+        """
         Finds an average from a list of degrees (cannot just be sum/len because of -179 and 179 will be 0, when it should be 180 or -180)
         deg: list of degrees ranging from -180 to 180
-        '''
+        :returns: the mean heading
+        """
         return math.degrees(phase(sum(rect(1, math.radians(d)) for d in deg)/len(deg)))
